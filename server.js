250b53ef99ecd9e4a822685573cd802972cc98f3
Chen,Shang-Kuei <adavis10006@gamil.com>
2017-11-01 13:25:27 +0800

    Add a markdown-it plugin manager and change highlight engine to PrismJS.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var path = require('path')
 var fs = require('fs')
 
+var dirResolve = require('dir-resolve');
+
+var yaml = require('js-yaml');
+var config = yaml.load(fs.readFileSync(__dirname + "/plugins.yml"));
+
 var express = require('express')
 var http = require('http')
 var socket = require('socket.io')
@@ -8,25 +13,222 @@ var chokidar = require('chokidar')
 var parser = require('body-parser')
 var request = require('request')
 var markdownIt = require('markdown-it')
-var markdownItTaskCheckbox = require('markdown-it-task-checkbox')
-var markdownItEmoji = require('markdown-it-emoji')
-var markdownItGitHubHeadings = require('markdown-it-github-headings')
+var markdownItUtils = require('markdown-it/lib/common/utils');
 
-var md = markdownIt({
-  html: true,
-  linkify: true
-})
-md.use(markdownItTaskCheckbox)
-md.use(markdownItEmoji)
-md.use(markdownItGitHubHeadings, {
-  prefix: ''
-})
+const themeRegex = /^prism-(.*).css$/;
+const codeRegex = /<pre><code(.*)?>([\s\S]*?)<\/code><\/pre>/igm;
+const captionRegex = /<p><code>(?![\s\S]*<code)(.*?)\s(.*?)\n([\s\S]*)<\/code><\/p>/igm;
+
+const map = {
+  '&#39;': '\'',
+  '&amp;': '&',
+  '&gt;': '>',
+  '&lt;': '<',
+  '&quot;': '"'
+};
+
+function unescape(str) {
+  if (!str || str === null) return '';
+  const re = new RegExp('(' + Object.keys(map).join('|') + ')', 'g');
+  return String(str).replace(re, (match) => map[match]);
+}
+
+function toThemeMap(basePath, filename) {
+  const matches = filename.match(themeRegex);
+  if (!matches)
+    return;
+
+  return {
+    name: matches[1],
+    filename,
+    path: path.join(basePath, filename)
+  };
+}
+
+function checkPrismPlugins(plugin) {
+  var _t = [];
+  for(var i = 0; i < plugin.length;i ++) {
+    if(!(plugin[i] instanceof Object) || !(plugin[i].plugin instanceof Object)) continue;
+    var pug_name = plugin[i].plugin.name;
+    if(!pug_name) continue;
+    if(plugin[i].plugin.css == null || plugin[i].plugin.css == undefined || plugin[i].plugin.css != true)
+      plugin[i].plugin.css = false;
+    if(plugin[i].plugin.enable == null || plugin[i].plugin.enable == undefined || plugin[i].plugin.enable != true)
+      plugin[i].plugin.enable = false;
+    else _t.push(plugin[i].plugin);
+  }
+
+  return _t;
+}
+
+const prismThemeDir = dirResolve('prismjs/themes');
+const extraThemeDir = dirResolve('prism-themes/themes');
+const prismMainFile = require.resolve('prismjs');
+const standardThemes = fs.readdirSync(prismThemeDir)
+  .map(themeFileName => toThemeMap(prismThemeDir, themeFileName));
+const extraThemes = fs.readdirSync(extraThemeDir)
+  .map(themeFileName => toThemeMap(extraThemeDir, themeFileName));
+
+const themes = standardThemes.concat(extraThemes).filter(Boolean);
+themes.push({
+  name: 'default',
+  filename: 'prism.css',
+  path: path.join(prismThemeDir, 'prism.css')
+});
+
+// Plugin settings from config
+const prismThemeName = config.prismJS.theme || 'default';
+const prismPlugins = checkPrismPlugins(config.prismJS.plugins);
+const prismTheme = themes.find(theme => theme.name === prismThemeName);
+const prismThemeFileName = prismTheme.filename;
+const prismThemeFilePath = prismTheme.path;
+
+function PrismPlugin(data) {
+  return data.replace(codeRegex, (origin, code_data, code) => {
+    var lang = "none";
+    if (code_data != undefined)
+      lang = code_data.replace(/.*language-(.*?)".*/, (origin, language) => {
+        return language});
+    else
+      code_data = ' class="language-none"'
+    const startTag = `<pre${code_data}><code class="language-${lang}">`;
+    const endTag = `</code></pre>`;
+    code = unescape(code);
+    return startTag + code + endTag;
+  });
+
+  return data;
+}
+
+function copyAssets() {
+  const assets = [{
+    path: path.join(__dirname, `public/css/${prismThemeFileName}`),
+    data: () => fs.createReadStream(prismThemeFilePath)
+  }];
+
+  prismPlugins.forEach(function (plugin) {
+    if(plugin.enable && plugin.css) {
+      assets.push({
+          path: path.join(__dirname, 'public/css/prism-' + plugin.name + '.css'),
+          data: () => fs.createReadStream(path.join(dirResolve('prismjs/plugins/' + plugin.name), 'prism-' + plugin.name + '.css'))
+      });
+    }
+  });
+
+  // If prism plugin config mode is realtime include prism.js and line-numbers.js
+  assets.push({
+    path: path.join(__dirname, 'public/js/prism.min.js'),
+    data: () => fs.createReadStream(prismMainFile)
+  });
+  prismPlugins.forEach(function (plugin) {
+    if(plugin.enable) {
+      assets.push({
+        path: path.join(__dirname, 'public/js/prism-' + plugin.name + '.min.js'),
+        data: () => fs.createReadStream(path.join(dirResolve('prismjs/plugins/' + plugin.name), 'prism-' + plugin.name + '.min.js'))
+      });
+    }
+  });
+
+  assets.forEach(function (plugin) {
+    plugin.data().pipe(fs.createWriteStream(plugin.path));
+  });
+}
+
+function importStyle() {
+  const css = [
+    '<link rel="stylesheet" href="/vendor/github-markdown.css">',
+    '<link rel="stylesheet" href="/style.css">',
+    `<link rel="stylesheet" href="/css/${prismThemeFileName}" type="text/css">`
+  ];
+
+  prismPlugins.forEach(function (plugin) {
+    if(plugin.enable && plugin.css) {
+      css.push('<link rel="stylesheet" href="/css/prism-' + plugin.name + '.css" type="text/css">');
+    }
+  });
+
+  return css.join("\n");
+}
+
+function importJavascript() {
+  const js = [
+    '<script src="/js/prism.min.js" defer></script>'
+  ];
+
+  js.push();
+  prismPlugins.forEach(function (plugin) {
+    if(plugin.enable) {
+      js.push('<script src="/js/prism-' + plugin.name + '.min.js" defer></script>');
+    }
+  });
+
+  return js.join("\n");
+}
+
+function checkValue(config, res, key, trueVal, falseVal) {
+    res[key] = (config[key] == true || config[key] == undefined || config[key] == null) ? trueVal: falseVal;
+}
+
+function checkConfig(config, md) {
+    var _res = {};
+    checkValue(config, _res, 'highlight', function(str, lang) {
+        return '<pre><code class="language-' + lang + '">' + markdownItUtils.escapeHtml(str) + '</code></pre>';
+        // return '';
+    }, function(str, lang) {
+        return '';
+    });
+    checkValue(config, _res, 'html', true, false);
+    checkValue(config, _res, 'xhtmlOut', true, false);
+    checkValue(config, _res, 'breaks', true, false);
+    checkValue(config, _res, 'linkify', true, false);
+    checkValue(config, _res, 'typographer', true, false);
+    _res['langPrefix'] = config['langPrefix'] ? config['langPrefix'] : '';
+    _res['quotes'] = config['quotes'] ? config['quotes'] : '“”‘’';
+    return _res;
+}
+
+var parseConfig = checkConfig(config.MarkdownIt);
+var md = require('markdown-it')(parseConfig);
+
+function checkMarkdownPlugins(plugin) {
+  var _t = [];
+  for(var i = 0;i < plugin.length;i++) {
+    if(!(plugin[i] instanceof Object) || !(plugin[i].plugin instanceof Object)) continue;
+    var plugin_name = plugin[i].plugin.name;
+    if(!plugin_name) continue;
+    if(plugin[i].plugin.enable == null || plugin[i].plugin.enable == undefined || plugin[i].plugin.enable != true)
+      plugin[i].plugin.enable = false;
+    _t.push(plugin[i].plugin);
+  }
+
+  return _t;
+}
+
+md = checkMarkdownPlugins(config.MarkdownIt.plugins).reduce(function (md, plugin) {
+  if(plugin.enable) {
+    if(plugin.name == 'markdown-it-toc-and-anchor') {
+      if(plugin.options != undefined)
+        return md.use(require('markdown-it-toc-and-anchor').default, plugin.options);
+      return md.use(require('markdown-it-toc-and-anchor').default);
+    }
+    if(plugin.options != undefined)
+      return md.use(require(plugin.name), plugin.options);
+    return md.use(require(plugin.name));
+  }
+  else return md;}, md);
 
 var app = express()
 var server = http.Server(app)
 var io = socket(server)
 
 var utils = require('./utils')
+
+
+function filePrepare() {
+  return fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8')
+    .replace('$prismJS', importJavascript())
+    .replace('$style', importStyle())
+}
 
 module.exports = function (opts) {
   return new Server(opts)
@@ -51,7 +253,7 @@ function Server (opts) {
       fs.readFile(path, 'utf8', function (err, data) {
         if (err) throw err
         data = data || ''
-        self.sock.emit('content', md.render(data))
+        self.sock.emit('content', PrismPlugin(md.render(data)))
       })
     })
   }
@@ -84,13 +286,13 @@ Server.prototype.start = function (filePath, next) {
     fs.readFile(filePath, 'utf8', function (err, data) {
       if (err) throw err
       data = data || ''
-      self.sock.emit('content', md.render(data))
+      self.sock.emit('content', PrismPlugin(md.render(data)))
     })
   })
 
   app.use(parser.json())
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.send(filePrepare())
   })
   app.use(express.static(path.join(__dirname, 'public')))
   app.use(express.static(path.dirname(filePath)))
